2f00e10f91dd699094e3cd5ad3ff01a122215bd0
Willy Schultz <will627@comcast.net>
2015-11-20 13:13:13 -0500

    Add more details on chessmodel layout and notes for rules engine

--- chessmodel.ml
+++ chessmodel.ml
@@ -1,8 +1,9 @@
 
 (* 
 
-	Chess Board Square Layout
+[ BOARD SQUARE ADDRESSING ]
 
+	-------- Black --------
 	a8 b8 c8 d8 e8 f8 g8 h8
 	a7 b7 c7 d7 e7 f7 g7 h7
 	a6 b6 c6 d6 e6 f6 g6 h6
@@ -11,6 +12,19 @@
 	a3 b3 c3 d3 e3 f3 g3 h3
 	a2 b2 c2 d2 e2 f2 g2 h2
 	a1 b1 c1 d1 e1 f1 g1 h1 
+	-------- White --------
+
+	and numerical (easier for move computations):
+	-------- Black --------
+	18 28 38 48 58 68 78 88
+	17 27 37 47 57 67 77 87
+	16 26 36 46 56 66 76 86
+	15 25 35 45 55 65 75 85
+	14 24 34 44 54 64 74 84
+	13 23 33 43 53 63 73 83
+	12 22 32 42 52 62 72 82
+	11 21 31 41 51 61 71 81 
+	-------- White --------
 
 Addressing a square: 
 
@@ -19,19 +33,48 @@ Addressing a square:
 
  	board is an association list that maps (row_index:string -> row:row)
 	row is an association lists that maps (col_index:string -> square ref)
+
+
+[ PIECE LABELINGS ]
+
+
+	------- Black ---------
+	R2 K2 B2 Q  K  B1 K1 R1
+	P8 P7 P6 P5 P4 P3 P2 P1
+	-- -- -- -- -- -- -- --
+	-- -- -- -- -- -- -- --
+	-- -- -- -- -- -- -- --
+	-- -- -- -- -- -- -- --
+	P1 P2 P3 P4 P5 P6 P7 P8
+	R1 K1 B1 Q  K  B2 K2 R2
+	------- White ---------
+
+	* each piece will also have
+	a "team" associated with it i.e. Black | White
+
+
 *)
 
+type team = White | Black
 
+type piecekind = 
+	| Pawn
+	| Knight
+	| Bishop
+	| Rook
+	| King
+	| Queen
 
 type piece = {
-		id: string; (* each piece will be identified by a unique id *)
+		id: string; (* each piece will be identified by a unique id e.g. P4 or B2 *)
+		team: team; (* black or white *)
 		name: string; (* piece type *)
-		team: string; (* black or white *)
+		piecetype: piecekind;
 	} 
 
-type boardpos = string*string (* (row,column) e.g. (5,a) *)
-type move = string * string (* descriptive chess notation (?), or algebraic chess notation? *)
-type square = boardpos * piece option (* (column letter, piece) *)
+type boardpos = string * string (* (row,column) e.g. (5,a) *)
+type move = piece * boardpos * boardpos  (* represents a PHYSICAL move --> (piece, src, dest) *)
+type square = boardpos * piece option (* (position,piece); piece=None if the suare is empty *)
 type row = (string * square ref) list (* (row #, list of squares) *)
 type board = (string * row) list 
 
--- rules.ml
+++ rules.ml
@@ -7,4 +7,52 @@ let possible_movements (p:piece) (brd:board) : move list =
 	failwith "possible_movements unimplemented"
 
 
-(* TODO: IMPLEMENT RULES ENGINE *)
+(* rule composition operator *)
+let (>>>) = failwith "no compose operator yet"
+
+type move_fxn = fun (int*int)
+
+type rule_phys = move_fxn * string
+
+(* 
+
+TODO: IMPLEMENT RULES ENGINE 
+
+Deadline:
+
+Finish rules engine by November 30
+
+
+Types of Rules:
+
+1. Physical Piece Movements
+	- Depends on:
+		-> Current Position (X,Y)
+		-> Destination (X',Y')
+	- Will not factor in other board pieces
+
+2. Game State Specific
+	a. En Passant
+	b. Castling
+	c. Pawn Promotion
+	d. Check, Checkmate
+
+3. Capture Rules
+	- Barring special rules, a piece can "capture" another piece if it can 
+	validly move to that space as per positioning rules, and the piece is of opposing
+	team
+
+
+
+Rule Checking:
+
+	physical |> game_state_rules |> capture rules |> bool
+
+
+
+
+
+
+
+
+*)
