e7334c5ec6f66970dfa60dceb2c846781f2f40f9
Bomi Lee <bl557@cornell.edu>
2015-11-29 13:16:05 -0500

    eval.ml and position_score initial push

--- eval.ml
+++ eval.ml
@@ -1,7 +1,6 @@
-(* 
- * The value 'eval board move' returns an integer representing the strength 
- * of the inputted move. Positive values represent strong moves for
- * white, while negative values represent strong moves for black.
+(*
+ * The value 'eval board move' returns an integer representing the strength
+ * of the inputted move.
  *
  * MOVES are evaluated rather than BOARD STATE for the sake of efficiency
  *
@@ -9,14 +8,19 @@
  * I. Captured pieces - point value of captured piece
  * II. Piece positioning - net gain in positional advantage
  * III. Piece vulnerabilities - number of and point values of vulnerable pieces
- * IV. Misc - misc stuff, such as castled formation, which piece is being moved
+ * IV. Misc - misc stuff, such as castled formation, piece is being moved, check,
+ *        pawn moved to end, avoidance of check mate
  *
  * INVARIANT: THE INPUTTED MOVE --MUST-- BE A VALID MOVE
  *)
 
-(* Notes to self: ask nate for captured pieces thing *)
+(* Notes to self: ask nate for pieces points
+   remove positional points if piece is vulnerable?
+   DETERMINE EARLY OR LATE GAME STATUS FOR KING'S POS POINTS
+ *)
 
-open chessmodel
+open Chessmodel
+open Position_score
 
 (* multipliers representing the weighting of the 3 board eval factors;
  * to be adjusted in testing *)
@@ -25,21 +29,47 @@ let position_mult = 1.0
 let vulnerable_mult = 1.0
 let misc_mult = 1.0
 
-let capture_points (board: board) (move: move) : int =
-	match move with
-	| (_, _, dest) ->
-	 match get_square_on_board dest board with
-	 | (_, piece) ->
+(* relative value of each piece- values derived from wikipedia *)
+let get_piece_val (piece: piece): float =
+  match piece.piecetype with
+  | Pawn -> 100.
+  | Knight -> 300.
+  | Bishop -> 300.
+  | Rook -> 500.
+  | Queen -> 900.
+  | King -> failwith ""
 
+let get_piece (board: board) (loc: boardpos): piece option =
+  snd (!get_square_on_board loc board)
 
-let position_points (board: board) (move: move) : int = failwith "TODO"
+(* points gathered from a piece capture *) (* change to use get piece *)
+let capture_points (board: board) (move: move) : float =
+  match move with
+  | (_, _, dest) ->
+   match !(get_square_on_board dest board) with
+   | (_, pieceOp) ->
+    match pieceOp with
+    | None -> 0
+    | Some piece -> get_piece_val piece
 
-let vulnerable_points (board: board) (move: move) : int = failwith "TODO"
+let position_points (board: board) (move: move) : float =
+  match move with
+  | (piece, src, dest) -> (get_pos_points piece.piecetype dest true) +
+    (match get_piece board dest with
+     | None -> 0
+     | Some cap_piece -> get_pos_points cap_piece.piecetype dest true)
 
-let misc_points (board: board) (move: move) : int = failwith "TODO"
+let vulnerable_points (board: board) (move: move) : float =
+  match move with
+  | (piece, _, dest) ->
+    if (is_vulnerable dest board)
+    then get_piece_val piece
+    else 0.
 
-let eval (board: board) (move: move) : int = 
-	(capture_mult * (capture_points board move)) + 
-	(position_mult * (position_points board move)) + 
-	(vulnerable_mult * (vulnerable_points board move)) + 
-	(misc_mult * (misc_points board move))
+let misc_points (board: board) (move: move) : float = 0.
+
+let eval (board: board) (move: move) : float =
+  (capture_mult * (capture_points board move)) +
+  (position_mult * (position_points board move)) -
+  (vulnerable_mult * (vulnerable_points board move)) +
+  (misc_mult * (misc_points board move))
---
+++ position_score.ml
@@ -0,0 +1,87 @@
+(*
+ * The position_score scores the positional advantages gained or lost
+ * through the movement/capture of a piece. Pawns, knights, bishops,
+ * and the king have tables representing the strength of the position
+ * of the piece.
+ *
+ * While the idea to score positional advantages was our own idea, the
+ * scoring of the positions of the pieces were taken from online (as
+ * our group's knowledge of chess is limited). The table scoring was
+ * taken from the following link:
+ * http://www.chessbin.com/post/Piece-Square-Table
+ *)
+
+open Chessmodel
+
+type pos_board = int Array Array
+
+let create_board (board: pos_board) (vals: int list) (row: int) (col: int): pos_board =
+  match vals with
+  | [] -> board
+  | h::t -> board.col.row <- h;
+        if (col = 7) then create_board board t (row-1) 0
+        else create_board board t row (col+1)
+
+let pawn_vals = [ 0.;  0.;  0.;  0.;  0.;  0.;  0.;  0.;
+          50.; 50.; 50.; 50.; 50.; 50.; 50.; 50.;
+          10.; 10.; 20.; 30.; 30.; 20.; 10.; 10.;
+           5.;  5.; 10.; 27.; 27.; 10.;  5.;  5.;
+           0.;  0.;  0.; 25.; 25.;  0.;  0.;  0.;
+           5.; -5.;-10.;  0.;  0.;-10.; -5.;  5.;
+           5.; 10.; 10.;-25.;-25.; 10.; 10.;  5.;
+           0.;  0.;  0.;  0.;  0.;  0.;  0.;  0.]
+
+let knight_vals = [-50.;-40.;-30.;-30.;-30.;-30.;-40.;-50.;
+            -40.;-20.;  0.;  0.;  0.;  0.;-20.;-40.;
+            -30.;  0.; 10.; 15.; 15.; 10.;  0.;-30.;
+            -30.;  5.; 15.; 20.; 20.; 15.;  5.;-30.;
+            -30.;  0.; 15.; 20.; 20.; 15.;  0.;-30.;
+            -30.;  5.; 10.; 15.; 15.; 10.;  5.;-30.;
+            -40.;-20.;  0.;  5.;  5.;  0.;-20.;-40.;
+            -50.;-40.;-20.;-30.;-30.;-20.;-40.;-50.]
+
+let bishop_vals = [-20.;-10.;-10.;-10.;-10.;-10.;-10.;-20.;
+            -10.;  0.;  0.;  0.;  0.;  0.;  0.;-10.;
+            -10.;  0.;  5.; 10.; 10.;  5.;  0.;-10.;
+            -10.;  5.;  5.; 10.; 10.;  5.;  5.;-10.;
+            -10.;  0.; 10.; 10.; 10.; 10.;  0.;-10.;
+            -10.; 10.; 10.; 10.; 10.; 10.; 10.;-10.;
+            -10.;  5.;  0.;  0.;  0.;  0.;  5.;-10.;
+            -20.;-10.;-40.;-10.;-10.;-40.;-10.;-20.]
+
+let king_vals_early = [-30.; -40.; -40.; -50.; -50.; -40.; -40.; -30.;
+               -30.; -40.; -40.; -50.; -50.; -40.; -40.; -30.;
+               -30.; -40.; -40.; -50.; -50.; -40.; -40.; -30.;
+               -30.; -40.; -40.; -50.; -50.; -40.; -40.; -30.;
+               -20.; -30.; -30.; -40.; -40.; -30.; -30.; -20.;
+                 -10.; -20.; -20.; -20.; -20.; -20.; -20.; -10.;
+                20.;  20.;   0.;   0.;   0.;   0.;  20.;  20.;
+              20.;  30.;  10.;   0.;   0.;  10.;  30.;  20.]
+
+let king_vals_late = [-50.;-40.;-30.;-20.;-20.;-30.;-40.;-50.;
+              -30.;-20.;-10.;  0.;  0.;-10.;-20.;-30.;
+              -30.;-10.; 20.; 30.; 30.; 20.;-10.;-30.;
+              -30.;-10.; 30.; 40.; 40.; 30.;-10.;-30.;
+              -30.;-10.; 30.; 40.; 40.; 30.;-10.;-30.;
+              -30.;-10.; 20.; 30.; 30.; 20.;-10.;-30.;
+              -30.;-30.;  0.;  0.;  0.;  0.;-30.;-30.;
+              -50.;-30.;-30.;-30.;-30.;-30.;-30.;-50.]
+
+let pawn_board = create_board (Array.make_matrix 8 8 0) pawn_vals 7 0
+let knight_board = create_board (Array.make_matrix 8 8 0) knight_vals 7 0
+let bishop_board = create_board (Array.make_matrix 8 8 0) bishop_vals 7 0
+let king_board_early = create_board (Array.make_matrix 8 8 0) king_vals_early 7 0
+let king_board_late = create_board (Array.make_matrix 8 8 0) king_vals_late 7 0
+
+let get_pos_points (piece: piece) (move: boardpos) (early: bool): float =
+  let pos = (match piece.team with
+        | White -> ((get_int_of_letter (snd move)) - 1, (int_of_string (fst move)) - 1)
+        | Black -> ((get_int_of_letter (snd move)) - 1, 8 - (int_of_string (fst move)))) in
+  match piece.piecetype with
+  | Pawn -> pawn_board.(fst pos).(snd pos)
+  | Knight -> knight_board.(fst pos).(snd pos)
+  | Bishop -> bishop_board.(fst pos).(snd pos)
+  | Rook -> 0
+  | King -> if early then king_board_early.(fst pos).(snd pos)
+        else king_board_late.(fst pos).(snd pos)
+  | Queen -> 0
