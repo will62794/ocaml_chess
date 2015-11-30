896a1811c5d90b332bedb72b5df8b15e19beecaf
Willy Schultz <will627@comcast.net>
2015-11-30 01:11:56 -0500

    Add unicode lib reference

--- display.ml
+++ display.ml
@@ -1,6 +1,10 @@
 (*Unused*)
 
-(* Chess Unicode Characters (might need help from a 3rd party lib?) *)
+(* 
+
+	Chess Unicode Characters (might need help from a 3rd party lib?) 
+	- https://github.com/ytomino/unicode-ocaml 
+*)
 
 let white_pcs = [("King","♔");("Queen","♕");("Bishop","♗");("Rook","♖");("Knight","♘");("Pawn","♙")]
 let black_pcs = [("King","♚");("Queen","♛");("Bishop","♝");("Rook","♜");("Knight","♞");("Pawn","♟")]
