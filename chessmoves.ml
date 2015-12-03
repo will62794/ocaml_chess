384795842f0459416c5bf5cd475cab1e6f6fed56
Nathaniel Rogalskyj <nr282@cornell.eduga68bRedgit config --global user.email nr282@cornell.edugit config --global user.name Nathaniel>
2015-12-02 22:00:46 -0500

    Began working on chessmoves and implmeneted file minmax, which houses the minmax algorithm written by Bomi. Neither chessmoves or minmax compile

--- chessmoves.ml
+++ chessmoves.ml
@@ -3,5 +3,36 @@
 open Chesstypes
 
 
+(*Given a movetype representing an action on the game board, update_game_with
+move, returns a new copy of the game with all the appropiate pieces moved. Deep
+copy. Updates the score
+
+type game = {
+  board: board;
+  players: player * player;
+  total_moves: int;
+  current_turn: team;
+  in_enpassant: (piece option) list;
+  did_castle: bool * bool; (* did_white_castle_yet, did_black_castle_yet *)
+  moved_pieces: piece list;
+}
+
+update_game_with_move returns a game with  a new game with all the appropiate
+fields updated
+
+
+
+type movetype = Basic | Capture | EnPassant | CastlingLeft | CastlingRight | PawnPromotion
+
+
+*)
+
+
 let update_game_with_move (movetyp:movetype) (move:move) (game:game) : game =
-  game
+  match movetyp with
+  |Basic ->
+  |Capture->
+  |EnPassant->
+  |CastlingLeft
+  |CastlingRight->
+  |PawnPromotion->
---
+++ minmax.ml
@@ -0,0 +1,28 @@
+open Move_tree
+
+let max (lst: (score * move option) list): (score * move option) =
+  List.fold_left (fun acc, hd -> if(fst acc > fst hd) then acc else hd)
+                 (min_float, None) lst
+
+let min (lst: (score * move option) list): (score * move option) =
+  List.fold_left (fun acc, hd -> if(fst acc < fst hd) then acc else hd)
+                 (max_float, None) lst
+
+(* make this tail recursive *)
+let rec get_list_of_children (tree_list: move_tree list)
+                              (levels: int) (team: team): (score * move option) list =
+  match tree_list with
+  | [] -> []
+  | h::t -> match h with
+            | Leaf -> failwith ""
+            | Node ((score, game, move), nodes_tree_list) ->
+                (min_max h levels team)::(get_list_of_children t levels team)
+
+and min_max (tree: move_tree) (levels: int) (team: team): (score * move option) =
+  match tree with
+  | Leaf -> failwith ""
+  | Node ((score, game, move), tree_list) ->
+    if (levels = 0) then (score, move)
+    else match team with
+         | White -> max (get_list_of_children (tree_list) (levels-1) Black)
+         | Black -> min (get_list_of_children (tree_list) (levels-1) White)
