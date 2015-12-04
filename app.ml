62ecda818c1371c203eb2950247439e9c5567b74
William Ford <wbf32@cornell.edu>
2015-12-03 19:56:18 -0500

    updated app.ml

--- app.ml
+++ app.ml
@@ -1,20 +1,24 @@
 open Chesstypes
+open Chessmodel
 open Cmdparse
 open Game
 
 
-let rec game_input () =
+let rec game_input g =
   let i = parse_cmd(read_line()) in
     match i with
-      | Quit -> (("0","0"),("0","0"))
-      | Move(x,y) -> (x,y)
-      | _ -> print_endline("please enter a valid command"); game_input()
+      | Quit -> exit 0
+      | Move(x,y) -> let (bpos,pc') = !(get_square_on_board x g.board) in
+                    begin
+                     match pc' with
+                      | None -> print_endline("please enter a valid move");
+                                game_input g
+                      | Some pc -> (pc,x,y)
+                    end
+      | _ -> print_endline("please enter a valid command"); game_input g
 
 let game_loop g gt =
-  let (p1move_r,p1move_c) = game_input() in
-  if (p1move_r,p1move_c) = (("0","0"),("0","0"))
-  then ()
-  else ()
+  ()
 
 
 let rec main_input () =
@@ -24,8 +28,8 @@ let rec main_input () =
                     print_endline("Starting PvP Game");
                     print_endline("");
                     print_endline("Enter Moves in the format:");
-                    print_endline("<SourceRow><SourceColumn> to" ^
-                                  "<DestinationRow><Column>" ^
+                    print_endline("<SourceRow><SourceColumn> to " ^
+                                  "<DestinationRow><Column> " ^
                                   "or type Quit to exit");
                     game_loop (make_game()) (true)
       | StartPvAI -> print_endline("");
@@ -35,8 +39,8 @@ let rec main_input () =
                      print_endline("<SourceRow><SourceColumn> to " ^
                                   "<DestinationRow><DestinationColumn> " ^
                                   "or type Quit to exit");
-                     game_loop (make_game) (false)
-      | Quit -> ()
+                     game_loop (make_game()) (false)
+      | Quit -> exit 0
       | Move(a,b) -> print_endline("please enter a valid command");
                      main_input()
       | InvalidCmd -> print_endline("please enter a valid command");
