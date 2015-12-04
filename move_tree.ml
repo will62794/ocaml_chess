a58060b7ec2649a7e4a8ec6a722e8b35e5fba246
Nathaniel Rogalskyj <nr282@cornell.eduga68bRedgit config --global user.email nr282@cornell.edugit config --global user.name Nathaniel>
2015-12-04 18:42:10 -0500

    Added branching factor to move_tree.ml. Spec for building a move tree has changed. Note for Testing and App.ml

--- move_tree.ml
+++ move_tree.ml
@@ -61,21 +61,34 @@ let copy_game game =
   in copy_game
 
 
+let rec get_first_n inc_to_dec_list n =
+  if n=0 then [] else
+  List.hd inc_to_dec_list :: get_first_n (List.tl inc_to_dec_list) (n-1)
 
-let rec generate_tree_helper (prev_num: float) (game:game) (levels:int) (move_before:move) : move_tree=
+
+let filter_list (thelist: (move* movetype* float) list) (percentage:float)=
+  let number_elements =  int_of_float (ceil (float_of_int((List.length thelist)) *. percentage)) in
+  let sorted_list = List.sort (fun a b ->
+  let (_,_,x) = a in let (_,_,y) = b in if x=y then 0 else if x>y then 1 else -1) thelist in
+  let rev_sorted_list = List.rev sorted_list in
+  get_first_n rev_sorted_list number_elements
+
+
+let rec generate_tree_helper (prev_num: float) (game:game) (levels:int) (move_before:move) (branching_factor:float) : move_tree=
 
   if levels=0 then Leaf else
   let all_possible_moves = get_all_possible_moves ((copy_game game).current_turn) (game) in
   let assoc_list = List.map (fun a -> (fst a , snd a , (eval (copy_game game) (fst a)))) all_possible_moves in
   (*[(move, move_type, float returned from eval)]*)
   (*update_game_with_move: move_type -> move -> game-> game*)
+  let assoc_list_2 =  filter_list assoc_list branching_factor in
   let produce_game_float t = match t with
   | (move, move_typ, score) -> ((update_game_with_move move_typ move (copy_game game)) , score, move) in
-  let (game_float_move: (game*float*move) list) = List.map produce_game_float assoc_list in
+  let game_float_move = List.map produce_game_float assoc_list_2 in
   (*[(game , float, move)]*)
   let move_tree_list = List.map (fun a ->
     match a with
-    | (new_game, new_score, move_2) -> generate_tree_helper (prev_num +. new_score) (copy_game new_game) (levels -1) (move_2))
+    | (new_game, new_score, move_2) -> generate_tree_helper (prev_num +. new_score) (copy_game new_game) (levels -1) (move_2) (branching_factor))
      game_float_move in
   Node ((prev_num, (copy_game game), Some move_before) , move_tree_list)
 
@@ -84,18 +97,20 @@ let rec generate_tree_helper (prev_num: float) (game:game) (levels:int) (move_be
 
 (*Levels corresponds to how many steps down the tree it will go before
 terminating with Leaf*)
-let generate_tree (game:game) (levels:int) =
+let generate_tree (game:game) (levels:int) (branching_factor:float) =
 let all_possible_moves = get_all_possible_moves ((copy_game game).current_turn) (game) in
   let assoc_list = List.map (fun a -> (fst a , snd a , (eval (copy_game game) (fst a)))) all_possible_moves in
   (*[(move, move_type, float returned from eval)]*)
   (*update_game_with_move: move_type -> move -> game-> game*)
+
+ let assoc_list_2 =  filter_list assoc_list branching_factor in
   let produce_game_float t = match t with
   | (move, move_typ, score) -> ((update_game_with_move move_typ move (copy_game game)) , score, move) in
-  let (game_float_move: (game*float*move) list) = List.map produce_game_float assoc_list in
+  let (game_float_move: (game*float*move) list) = List.map produce_game_float assoc_list_2 in
   (*[(game , float, move)]*)
   let move_tree_list = List.map (fun a ->
     match a with
-    | (new_game, new_score, move_2) -> generate_tree_helper (0.0+.new_score) (copy_game new_game) (levels -1) (move_2))
+    | (new_game, new_score, move_2) -> generate_tree_helper (0.0+.new_score) (copy_game new_game) (levels -1) (move_2)(branching_factor))
      game_float_move in
   Node ((0.0, (copy_game game), None) , move_tree_list)
   (*[(move, move_type)]*)
@@ -132,7 +147,8 @@ let rec print_tree (move_tree: move_tree): unit =
 (*
 let _ =
 let init_game = make_game () in
-let thetree = generate_tree init_game 2 in
+let thetree = generate_tree init_game 2 0.5 in
 let _= print_tree thetree in
 ()
 *)
+
